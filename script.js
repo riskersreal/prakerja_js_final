cc0fde41d6610ef116da8e75123e2581302f4090
Remigius Real <remigius.real@gmail.com>
2023-07-23 18:14:28 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function mouseoverFooter() {
+    document.getElementById("footerCopy").style.fontFamily= "'Brush Script MT', cursive";
+  }
+  
+function mouseoutFooter() {
+    document.getElementById("footerCopy").style.fontFamily = "'Times New Roman', Times, serif";
+}
